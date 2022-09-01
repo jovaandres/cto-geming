5f3ed735f596717aeca58953fba02a75e1315627
Jova Andres <jovaandrea8721@gmail.com>
2022-09-01 14:01:01 +0700

    update lol

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -2,7 +2,9 @@ import IORedis from "ioredis";
 
 const redis = new IORedis({
   port: Number(process.env.REDIS_PORT),
-  host: process.env.REDIS_HOST
+  host: process.env.REDIS_HOST,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD
 });
 
 redis.on("ready", () => {
